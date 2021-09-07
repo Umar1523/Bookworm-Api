adaea7ff0a844abd51a25ba3ff6ecebac9c7dcab
Umar1523 <waseemexam1990@gmail.com>
2021-09-07 17:43:41 +0530

    Signup

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import  mongoose  from 'mongoose';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import Promise from 'bluebird';
-// import auth from "./routes/auth"
+
+import auth from "./routes/auth";
+import users from "./routes/users";
 
 
 dotenv.config();
@@ -14,10 +16,11 @@ app.use(bodyParser.json());
 mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB_URL)
 
-// app.use("/api/auth", auth)
-app.post('/api/auth', (req, res) => {
-    res.status(400).json({ errors: { global: "Invalid credentials" } });
-})
+app.use("/api/auth", auth);
+app.use("/api/users", users);
+// app.post('/api/auth', (req, res) => {
+//     res.status(400).json({ errors: { global: "Invalid credentials" } });
+// })
 
 app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));

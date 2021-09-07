88e927bfd9a5554f4ac2ab611947a1bcb7630b39
Umar1523 <waseemexam1990@gmail.com>
2021-09-07 21:20:00 +0530

    user email confirmation

--- src/models/User.js
+++ src/models/User.js
@@ -1,36 +1,46 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import uniqueValidator from 'mongoose-unique-validator';
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import uniqueValidator from "mongoose-unique-validator";
 
 // TODO: add uniqueness and email validations to email field
 const schema = new mongoose.Schema(
-    {
-        email: { 
-            type: String, 
-            required: true, 
-            lowercase: true, 
-            index: true, 
-            unique: true 
-        },
-        passwordHash: { type: String, required: true },
-        confirmed: {type: Boolean, default: false}
-    }, 
-    { timestamps: true }
+  {
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      index: true,
+      unique: true
+    },
+    passwordHash: { type: String, required: true },
+    confirmed: { type: Boolean, default: false },
+    confirmationToken: { type: String, default: "" }
+  },
+  { timestamps: true }
 );
 
 schema.methods.isValidPassword = function isValidPassword(password) {
-    return bcrypt.compareSync(password, this.passwordHash);
+  return bcrypt.compareSync(password, this.passwordHash);
 };
 
 schema.methods.setPassword = function setPassword(password) {
-    this.passwordHash = bcrypt.hashSync(password, 10);
-}
+  this.passwordHash = bcrypt.hashSync(password, 10);
+};
+
+schema.methods.setConfirmationToken = function setConfirmationToken() {
+    this.confirmationToken = this.generateJWT();
+};
+
+schema.methods.generateConfirmationUrl = function generateConfirmationUrl() {
+    return `${process.env.HOST}/confirmation/${this.confirmationToken}`;
+};
 
 schema.methods.generateJWT = function generateJWT() {
     return jwt.sign(
         {
-        email: this.email
+        email: this.email,
+        confirmed: this.confirmed
         },
         process.env.JWT_SECRET
     );
